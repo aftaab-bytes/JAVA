70584cfe389c9cbef665242aeb7914b1c1f18b1c
aftaab-bytes <aftaab.khimaninzb@gmail.com>
2024-09-01 19:01:39 +0530

    HELLO WORLD PROGRAM

---
+++ BASICS/hello_world.java
@@ -0,0 +1,11 @@
+
+// package com.company;
+public class hello_world {
+    public static void main(String[] args)
+    {
+        int a=1,b=4,c=14;
+        int sum = a+b+c;
+        System.out.println("HELLO WORLD");
+        System.out.println(sum);
+    }
+}
