723cd1bd71e6a7c27d101c2800971a68af2f76a5
aftaab-bytes <aftaab.khimaninzb@gmail.com>
2024-09-01 19:04:45 +0530

    EXAMPLE OF MATH METHOD

---
+++ BASICS/Math_method.java
@@ -0,0 +1,9 @@
+public class Math_method {
+ static void main(String[] args)
+    {
+        int a=64,b=81;
+        System.out.printf("%.2f %d %d ",Math.sqrt(a),Math.max(a,b),Math.min(a,b));
+        int ran = (int) (Math.random() * 14);
+        System.out.println(ran);
+    }
+}
