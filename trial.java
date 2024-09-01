282a4283122dcc9318f610b5f5b339eafcae5b7b
aftaab-bytes <aftaab.khimaninzb@gmail.com>
2024-09-01 19:06:05 +0530

    ADDING .class files

---
+++ trial.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+public class trial {
+    public static void main(String[] args)
+    {
+        HashSet<String> obj = new HashSet<String>();
+        obj.add("asdf");
+        obj.add("Aftaab");
+        obj.add("asdf");// this is not added since same is already added in the hashset we can make a duplicate member in it all the members are unique to each other
+        for (String i : obj)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
