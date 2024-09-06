ee2743ceb43547dcf55e71fcdc17a910cae348d2
aftaab-bytes <aftaab.khimaninzb@gmail.com>
2024-09-06 23:33:31 +0530

    POLYMORPHISM

---
+++ NEW_THINGS/Polymorphism/vehicle.java
@@ -0,0 +1,22 @@
+class vehicle {
+    String brand = null;
+    void sound()
+    {
+        System.out.println("This is the sound in the vehicle class");
+    }    
+}
+class car extends vehicle{
+    String brand = "BMW";
+    int sound()
+    {
+
+    }
+}
+class bike extends vehicle{
+    String brand = "YAMAHA";
+    void sound()
+    {
+        System.out.println("This is the sound of bike");
+    }
+}
+
